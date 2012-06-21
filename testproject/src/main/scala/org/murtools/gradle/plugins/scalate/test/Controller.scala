0f0bf1d089191eddbb590c3391de600602e90010
ryo-murai <murai.ry@gmail.com>
2012-06-21 20:24:10 +0900

    create new nested project for test

---
+++ testproject/src/main/scala/org/murtools/gradle/plugins/scalate/test/Controller.scala
@@ -0,0 +1,10 @@
+package org.murtools.gradle.plugins.scalate.test
+
+import org.fusesource.scalate._
+
+class Controller {
+	def execute(p : Person): String {
+		val te = new TemplateEngine
+		te.layout("user.ssp", Map("user"->p))
+	}
+}
---
+++ testproject/src/test/scala/org/murtools/gradle/plugins/scalate/test/ControllerTest.scala
@@ -0,0 +1,17 @@
+package org.murtools.gradle.plugins.scalate.test
+
+import org.junit._
+import Assert._
+
+class ControllerTest {
+	@Test def render {
+		val output = """<p>Hi scalate,</p>
+<p>1</p>
+<p>2</p>
+<p>3</p>
+<p>See, I can count!</p>"""
+
+		val controller = new Controller
+		assertEquals(controller.execute(new Person('scalate')), output)
+	}
+}
