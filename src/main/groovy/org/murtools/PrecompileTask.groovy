e1235bd31655177fa918c2642f80e774b8bb8409
ryo-murai <murai.ry@gmail.com>
2012-06-21 13:02:58 +0900

    add classpath of compile configuration and classes generated. still need set dependency on compileScala

--- src/main/groovy/org/murtools/PrecompileTask.groovy
+++ src/main/groovy/org/murtools/PrecompileTask.groovy
@@ -3,12 +3,11 @@ package org.murtools
 import groovy.util.logging.Slf4j;
 import groovy.util.Proxy
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.FileCollection;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.TaskAction
 
 @Slf4j
 class PrecompileTask extends DefaultTask {
-	FileCollection classpath
 	File templateSrcDir
 	File workingDirectory
 	File targetDirectory 
@@ -18,9 +17,8 @@ class PrecompileTask extends DefaultTask {
 	
 	@TaskAction
 	protected void start() {
-		debug()
-		
-		def precompiler = loadPrecompiler()
+		def classpathURLs = convertURL(project.files(project.configurations.compile, 'build/classes/main'))
+		def precompiler = loadPrecompiler(classpathURLs)
 		precompiler.invokeMethod('sources_$eq', templateSrcDir)
 		precompiler.invokeMethod('workingDirectory_$eq', workingDirectory)
 		precompiler.invokeMethod('targetDirectory_$eq', targetDirectory)
@@ -29,23 +27,20 @@ class PrecompileTask extends DefaultTask {
 		precompiler.invokeMethod('bootClassName_$eq', bootClassName)
 		precompiler.invokeMethod("execute", null);
 	}
-	
-	private GroovyObject loadPrecompiler() {
+
+	private GroovyObject loadPrecompiler(URL[] classpathURLs) {
 		def precompilerClassName = 'org.fusesource.scalate.support.Precompiler'
 
 		ClassLoader parent = Thread.currentThread().getContextClassLoader()
-		def precompiler = new URLClassLoader(convertURL(classpath), parent).loadClass(precompilerClassName).newInstance()
+		def precompiler = new URLClassLoader(classpathURLs, parent).loadClass(precompilerClassName).newInstance()
 		
 		return new Proxy().wrap(precompiler)
 	}
-	
-	private URL[] convertURL(FileCollection files) {
-		return files.collect {new URL(it.absolutePath)}
-	}
 
-	private void debug() {
-		System.out.println "$classpath is a " + classpath.class + ", contains " + classpath.files.size()
-		classpath.each  { System.out.println it }
+	private URL[] convertURL(FileCollection files) {
+		def paths = files*.path
+		println "$paths"
+		
+		return files.collect {it.toURL()}
 	}
-	
 }
